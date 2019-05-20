1c0ff09830dbf53cf1ab29c0f8ff75fba2dbab3c
shelmbreck <samantha.helmbreck@gmail.com>
2019-05-20 16:36:26 -0700

    updated Task.js, took out commented out code

--- src/pages/Task.js
+++ src/pages/Task.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Container, Row, Card, CardTitle, CardBody, Button, Col, Modal, ModalHeader, ModalBody, ModalFooter, Input, Label, Form  } from 'reactstrap';
 import {  Link } from 'react-router-dom';
-import { FaCity , FaEnvelopeSquare, FaSuitcase , FaTrash, FaWrench} from "react-icons/fa";
+import { FaTrash, FaWrench} from "react-icons/fa";
 import axios from 'axios';
 import SERVER_URL from '../constants/server';
 
@@ -83,12 +83,12 @@ class Task extends Component {
     axios.delete(`${SERVER_URL}/tasks/${this.props.id}`,
       {
         headers: {
-         'Authorization' : `Bearer ${token}`
-       }
-     })
+        'Authorization' : `Bearer ${token}`
+      }
+    })
     .then(response=> {
-     console.log("deleted", response);
-     this.props.rerender();
+    console.log("deleted", response);
+    this.props.rerender();
     })
     .catch(err => {
       console.log('error axios to server:');
@@ -103,7 +103,7 @@ class Task extends Component {
         if(Wanteduser.id == user){
           return  Wanteduser.image
         }
-     });
+    });
     let finalResult = result.filter(options => options != undefined)
       return finalResult[0]
   }
@@ -113,7 +113,7 @@ class Task extends Component {
         if(Wanteduser.id == user){
           return  Wanteduser.firstName
         }
-     });
+    });
     let finalResult = result.filter(options => options != undefined)
       return finalResult[0]
   }
@@ -195,32 +195,32 @@ class Task extends Component {
 
     return (
       <div className="tasks" id={this.props.id} draggable="true" onDragStart={this.drag} onDragOver={this.noAllowDrop} onDrop={this.changeState}>
-         <Card>
-           <CardTitle className="tasktools">
-             <div>
-               <Link draggable="false" onClick={() => this.handleDeleteTask()} >
-                 <FaTrash  id="deleteicon"/>
-               </Link>
-               <Link draggable="false" onClick={() => this.toggle()} >
-                 <FaWrench  id="modifyicon"/>
-               </Link>
-             </div>
-           </CardTitle>
-           <CardBody>
-             <div>
-               <Row>
-                 <Col>
-                   <img  draggable="false" id="usertask" src={ this.getPicture(this.props.task.assignedTo)}  />
-                     {this.getName(this.props.task.assignedTo)}
-                     {this.props.children}
-                 </Col>
-                 <Col>
-                   {this.props.task.title}
-                 </Col>
-               </Row>
-             </div>
-           </CardBody>
-         </Card>
+        <Card>
+          <CardTitle className="tasktools">
+            <div>
+              <Link draggable="false" onClick={() => this.handleDeleteTask()} >
+                <FaTrash  id="deleteicon"/>
+              </Link>
+              <Link draggable="false" onClick={() => this.toggle()} >
+                <FaWrench  id="modifyicon"/>
+              </Link>
+            </div>
+          </CardTitle>
+          <CardBody>
+            <div>
+              <Row>
+                <Col>
+                  <img  draggable="false" id="usertask" src={ this.getPicture(this.props.task.assignedTo)}  />
+                    {this.getName(this.props.task.assignedTo)}
+                    {this.props.children}
+                </Col>
+                <Col>
+                  {this.props.task.title}
+                </Col>
+              </Row>
+            </div>
+          </CardBody>
+        </Card>
 
         <Modal
           isOpen={this.state.modal}
@@ -282,14 +282,6 @@ class Task extends Component {
                 value={this.state.dateCompleted}
                 onChange={this.handleDateCompletedChange}
               />
-                {/*/   <Label>Prerequisite Tasks</Label>
-              // <Input
-              //   type="text"
-              //   name="prerequisiteTasks"
-              //   placeholder="related tasks"
-              //   value={this.state.prerequisiteTasks}
-              //   onChange={this.handlePrerequisiteTasksChange}
-              //     >*/}
               <Label>Description</Label>
               <Input
                 type="textarea"
@@ -306,41 +298,6 @@ class Task extends Component {
             </ModalFooter>
           </Form>
         </Modal>
-        {/*/ <div>
-        //   {8}
-        // </div>
-        // <div>
-        //   <div className="task-name">
-        //     <h2>Task X</h2>
-        //   </div>
-        //   <div className="task-details-imp" onClick={this.props.getUserProfInfo}>
-        //     Owner: This Person
-        //   </div>
-        //   <div className="task-priority">
-        //     Priority Level: 8
-        //   </div>
-        //   <div className="task-man-hour-budget">
-        //     Man Hours Required Estimate: 10
-        //   </div>
-        //   <div className="task-date-completed">
-        //     Completed on: 05/10
-        //   </div>
-        //   <div className="task-status">
-        //     In progress
-        //   </div>
-        //   // <div className="task-depends-on">
-        //   //   Prerequisite tasks: <a href="#">Task B</a>, <a href="#">Task C</a>
-        //   // </div>
-        //   <div className="task-desc">
-        //     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        //   </div>
-        //   <div className="task-tags">
-        //     Tags: HTML, CSS, Javascript
-        //   </div>
-        //   <div className="task-date-assigned">
-        //     Assigned on: 05/01
-        //   </div>
-        // </div> */}
       </div>
 
 
