4f48bb90e362c951a7aa59c65d318d1e8e7c4d6c
mtom92 <torresmoranm@gmail.com>
2019-05-20 21:12:36 -0700

    deleted unecessary code

--- src/pages/Task.js
+++ src/pages/Task.js
@@ -46,7 +46,7 @@ class Task extends Component {
   }
 
   componentDidMount = () => {
-    console.log(this.props.users);
+    console.log("this from component did mount task",this.props.users);
     this.setState({
       assignedTo: this.props.task.assignedTo,
       title: this.props.task.title,
@@ -306,41 +306,6 @@ class Task extends Component {
             </ModalFooter>
           </Form>
         </Modal>
-        {/*/ <div>
-        //   {8}
-        // </div>
-        // <div>
-        //   <div className="task-name">
-        //     <h2>Task X</h2>
-        //   </div>
-        //   <div className="task-details-imp" onClick={this.props.getUserProfInfo}>
-        //     Owner: This Person
-        //   </div>
-        //   <div className="task-priority">
-        //     Priority Level: 8
-        //   </div>
-        //   <div className="task-man-hour-budget">
-        //     Man Hours Required Estimate: 10
-        //   </div>
-        //   <div className="task-date-completed">
-        //     Completed on: 05/10
-        //   </div>
-        //   <div className="task-status">
-        //     In progress
-        //   </div>
-        //   // <div className="task-depends-on">
-        //   //   Prerequisite tasks: <a href="#">Task B</a>, <a href="#">Task C</a>
-        //   // </div>
-        //   <div className="task-desc">
-        //     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        //   </div>
-        //   <div className="task-tags">
-        //     Tags: HTML, CSS, Javascript
-        //   </div>
-        //   <div className="task-date-assigned">
-        //     Assigned on: 05/01
-        //   </div>
-        // </div> */}
       </div>
 
 
